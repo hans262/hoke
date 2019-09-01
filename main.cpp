f38c28d7ce2dc482a66d51b9acb388c8f51977f6
ouna <771565119@qq.com>
2019-09-01 22:33:16 +0800

    dwqdwq

--- main.cpp
+++ main.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-#include "./lib/test.h"
 
 int main(int argc, char const *argv[])
 {
+  //动态分配的数组
+  vector<string> arr{"Hello", "C++", "World", "from", "VS Code!"};
+  arr.push_back("huahua");
 
-  test();
+  //遍历数组 赋值给word，取word的内存地址
+  for (const string &word : arr)
+  {
+    cout << word << " ";
+  }
+  cout << endl;
+
+  cout << arr.size() << endl;
+  cout << arr[0] << endl;
+
+  struct Info
+  {
+    int moth;
+    int base[5];
+  };
+
+  vector<Info> arr2;
+
+  for (int i = 0; i < 12; i++)
+  {
+    struct Info info = {i + 1, {20, 15, 30, 40, 65}};
+    arr2.push_back(info);
+  }
+
+  for (const Info &bbw : arr2)
+  {
+    cout << bbw.base[2] << " ";
+  }
+
   return 0;
 }
