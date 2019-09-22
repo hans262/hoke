a6ffa333553b76c97bb9569407b1a0665ca0f70b
ouna <771565119@qq.com>
2019-09-22 22:42:12 +0800

    dqwdwq

--- go/fib/fibonacci.go
+++ go/fib/fibonacci.go
@@ -1,17 +1,19 @@
 package main
+
 import (
 	"fmt"
 	"time"
 )
+
 /**
  * 斐波那数列 递归测试
  * 40 420.6252ms
  * 45 4.6168056s
-*/
+ */
 
-func main()  {
+func main() {
 	var fib int = 45
-	start:=time.Now()
+	start := time.Now()
 	var ret int = fibonacci(fib)
 	end := time.Since(start)
 
@@ -20,8 +22,8 @@ func main()  {
 }
 
 func fibonacci(num int) int {
-	if num <= 2{
+	if num <= 2 {
 		return 1
 	}
-	return fibonacci(num - 1) + fibonacci(num - 2)
+	return fibonacci(num-1) + fibonacci(num-2)
 }
---
+++ go/go.md
@@ -0,0 +1,5 @@
+## 编译
+go build test.go
+
+## 脚本运行
+go run test.go
