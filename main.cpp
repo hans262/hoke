1a45b9fa6f5ea5fbac021f48c11fcb2efaac14e1
hans <771565119@qq.com>
2024-07-30 21:47:06 +0800

    1231

--- main.cpp
+++ main.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 int main() {
-  string name = "huahua";
-  cout << "hello world " << name << endl;
+  // string name = "huahua";
+  // cout << "hello world " << name << endl;
 
-  char str[6] = {'h', 'e', 'l', 'l', 'o', '\0'};
+  // char str[6] = {'h', 'e', 'l', 'l', 'o', '\0'};
 
-  printf("str = %s\n", str);
-  cout << str << endl;
+  // printf("str = %s\n", str);
+  // cout << str << endl;
+
+
+  int a = 10;
+  volatile int* p; //永远去拿取新值
+  p = &a;
+
+  // volatile int* b;
+  // b = &a;
+
+  cout << p << endl;
+
 }
